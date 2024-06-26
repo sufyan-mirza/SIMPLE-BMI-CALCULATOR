9cb8e78b4a65c0f9ff1573e26f0295e8ac318f6b
sufyan-mirza <160243704+sufyan-mirza@users.noreply.github.com>
2024-06-26 21:14:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// BMI calculator(simple)
+var weight = 19;
+var height = 22;
+var BMI = weight / height * height;
+console.log(BMI);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// BMI calculator(simple)
+
+let weight=19;
+let height=22;
+
+let BMI=weight/height*height
+
+console.log(BMI);
